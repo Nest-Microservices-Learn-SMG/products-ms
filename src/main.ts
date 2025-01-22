022a6b0414e754e8bae925f67c7bd586f8f8b5cf
Santiago Martinez <smartinez9626@gmail.com>
2025-01-22 16:19:14 -0500

    feat: add NestJS microservices support and update main bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
   const logger = new Logger('Main');
 
   app.useGlobalPipes(
@@ -14,7 +23,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.port);
-  logger.log(`Application listening on port ${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservice is running on: ${envs.port}`);
 }
 bootstrap();
