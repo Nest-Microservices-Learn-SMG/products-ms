2935e88d61f84e5808b1f8cc50e8313a4c6106c6
Santiago Martinez <smartinez9626@gmail.com>
2025-01-23 16:38:58 -0500

    feat: refactor ProductsController and ProductsService to use Payload decorator and RpcException for error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -52,7 +48,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product)
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
 
     return product;
   }
@@ -75,12 +74,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     await this.findOne(id);
 
     // return this.product.delete({
-    //   where: { id },
+    //   where: { id }
     // });
 
     const product = await this.product.update({
       where: { id },
-      data: { available: false },
+      data: {
+        available: false,
+      },
     });
 
     return product;
